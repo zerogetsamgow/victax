1e0a93a5f4b35c86269a575b5d7f06e273959a12
zerogetsamgow <sam.gow@gmail.com>
2024-10-03 11:09:30 +1000

    Initial commit

---
+++ app.R
@@ -0,0 +1,207 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(systemfonts)
+library(shiny)
+library(vagotheme)
+library(tidyverse)
+library(arrow)
+
+db =  dbConnect(duckdb())
+
+# Read data 
+victax.app = read_parquet( "./data/victax.parquet") |> 
+  filter(str_detect(tax_line,("Total|Payroll tax|Land ")))
+
+victax.app =  copy_to(db,   df = victax.app, overwrite = TRUE)
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    # Application title
+    titlePanel("Victorian Government tax revenue dashboard"),
+    br(),
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            # Input: Select the random distribution type ----
+            radioButtons(".taxline", "Tax line:", choices = unique(victax.app |> pull(tax_line))),
+            br(),
+            radioButtons(".addlevy", "Add levy revenue:", choices = c("Yes","No"),
+                          selected = "No"),
+            textOutput("explainer"),
+            br(),
+            checkboxGroupInput(
+              ".budget","Budget", 
+              choices = c("2019-20","2020-21","2021-22","2022-23","2023-24","2024-25"),
+              selected =  c("2021-22","2024-25"))
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+          plotOutput("taxplot"),
+          br(),
+          tableOutput("taxtable")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  # Get data 
+  .victax.app <<-
+    reactive({
+      if(input$.addlevy == "No" & 
+         input$.taxline %in% c("Payroll tax","Land tax")){
+        filter(
+          victax.app,
+          is.na(tax_sub)) |>  collect()
+      } else {
+        victax.app |> 
+          group_by(
+            financial_year,
+            tax_line,
+            estimate_type,
+            publication_year,
+            publication_type,
+            fy_date) |> 
+          summarise(estimate = sum(estimate)) 
+      }
+      })
+      
+  
+  .estimate.data <<-
+    reactive({
+      filter(
+        .victax.app(), 
+        tax_line == input$.taxline,
+        publication_type %in% c("Budget"),
+        publication_year %in% input$.budget
+     ) |> collect()
+    })
+  
+  .actual.data <<- 
+    reactive({ 
+      filter(
+        .victax.app(), 
+        tax_line == input$.taxline,
+        publication_type == "Actual",
+        fy_date > as.Date("2019-1-1")
+     ) |> collect()
+    })
+  
+  .min = reactive({max(0,floor(min(.actual.data()$estimate)/1e3)-1)})
+  .max = reactive({ceiling(max(.estimate.data()$estimate)/1e3)})
+  
+  
+  .colours =
+    c("2019-20" = vago_colours$theme[1],
+      "2020-21" = vago_colours$theme[2],
+      "2021-22" = vago_colours$theme[3],
+      "2022-23" = vago_colours$theme[4],
+      "2023-24" = vago_colours$theme[5],
+      "2024-25" = vago_colours$theme[6]
+    )
+  
+  output$explainer =
+    renderText({
+      str_c(
+        "Choosing Yes adds COVID and ",
+        "Mental health and wellbeing", 
+        "levies to Payroll tax and ",
+        "Land tax estimates.", sep = '\n')
+    })
+  
+  output$taxtable =
+    renderTable({
+      bind_rows(
+        .estimate.data(),
+        .actual.data()
+      ) |> 
+      ungroup() |> 
+      filter(financial_year %in% c("2022-23","2023-24")) |> 
+      arrange(publication_year, financial_year) |>
+      select(
+        "Source" = publication_type,
+        "Year" = publication_year,
+        financial_year,
+        estimate
+      ) |> 
+      pivot_wider(names_from = financial_year,values_from = estimate) |> 
+      data.table::data.table()},
+      spacing = "l",
+      digits = 0,
+      width = "80%",
+      na = "-"
+    )
+  
+  
+  output$taxplot =
+    renderPlot({
+      req(input$.taxline)
+      req(input$.budget)
+      suppressWarnings(
+        ggplot() +
+          geom_line(
+            data = .estimate.data(),
+            size = 1.2,
+            aes(
+              x = fy_date,
+              y = estimate/1000,
+              colour = publication_year,
+              group = publication_year,
+            )
+          )+
+        geom_text(
+          size = 5,
+          data = 
+            .estimate.data() |> 
+              group_by(publication_type,publication_year) |>
+              filter(fy_date == max(fy_date)),
+          aes(
+            x = fy_date + days(1),
+            y = estimate/1000,
+            label = publication_year,
+            colour = publication_year),
+          vjust = 0,
+          hjust = 0)+
+        geom_line(
+          size = 1.2,
+          data = .actual.data(),
+          aes(
+            x = fy_date,
+            y = estimate/1000,
+          ),
+          colour = vago.grey) +
+        scale_y_continuous(
+          name = "Revenue, $ billion",
+          limits = c(.min(),.max()),
+          #breaks = seq(0,40, by = 2)
+        )+
+        scale_x_date(
+          name = "Financial year ending 30 June",
+          breaks = 
+            seq.Date(
+              from = dmy("30-6-2019"),
+              to = dmy("30-6-2028"),
+              by = "years"),
+          date_labels = "%Y",
+          limits = dmy("1-6-2019","1-1-2030")) +
+        guides(colour = "none") +
+        labs(title = str_glue("Budget {str_to_lower(input$.taxline)} vs actual revenue (grey line)")) +
+        scale_color_manual(values = .colours) +
+        theme_vago_white()
+      
+    )
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ data-raw/Get Victorian tax data.R
@@ -0,0 +1,54 @@
+## code to prepare `DATASET` dataset goes here
+library(tidyverse)
+library(rvest)
+library(arrow)
+
+dtf.url = "https://www.dtf.vic.gov.au/"
+tax.url = str_c(dtf.url,"state-financial-data-sets/state-taxation-revenue")
+
+tax.file.url =
+  tax.url |> 
+  read_html() |> 
+  html_elements('div [href$="xlsx"]') |>
+  html_attr("href") |> 
+  as_tibble_col("link") |> 
+  filter(!str_detect(link,"Qtr")) |> 
+  as_tibble_col() |> 
+  mutate(value = str_c(dtf.url,value))
+
+tax.temp = tempfile(fileext = "xlsx")
+
+download.file(tax.file.url$value[1],tax.temp,mode="wb")
+
+victax =
+  tibble(
+    sheet=map(tax.temp,readxl::excel_sheets)
+  ) |> 
+  unnest(sheet) |> 
+  filter(!str_detect(sheet,"Introduction|Overview")) |> 
+  mutate(data=pmap(list(tax.temp,sheet), readxl::read_excel, skip=4)) |> 
+  unnest(data) |> 
+  rename(financial_year = ...1) |> 
+  select(sheet, financial_year, Revenue, contains("20")) |> 
+  pivot_longer(-sheet:-financial_year, names_to = "estimate_type", values_to = "estimate") |> 
+  mutate(
+    estimate = as.numeric(estimate),
+    estimate_type = str_replace(estimate_type,"Revenue","Actual"),
+    publication_year = str_extract(estimate_type, "[0-9]{4}-[0-9]{2}"),
+    publication_type = str_remove(estimate_type,  publication_year) |> str_trim(),
+    estimate_type = factor(estimate_type, levels = c("Actual","Estimate")),
+    estimate_type = coalesce(estimate_type,"Estimate") |> factor(),
+    publication_type = coalesce(publication_type,estimate_type,) |> factor(),
+    tax_sub = str_extract(sheet,".*levy"),
+    tax_line = str_remove(sheet, tax_sub) |> str_trim(),
+    tax_line = coalesce(tax_line,sheet),
+    tax_line = str_replace(tax_line, ".*(payroll|wellbeing).*","Payroll tax"),
+    tax_line = str_replace(tax_line, ".*landholding.*","Land tax")) |> 
+  filter(!is.na(estimate)) |> 
+  mutate(fy_date = fy::fy2date(financial_year)) 
+
+write_parquet(
+  victax,
+  sink = "./data/victax.parquet"
+)
+
