9dd1318f285e0894aaabcbca5d7077d7a16847a4
Sam Gow <sam.gow@gmail.com>
2026-05-05 15:16:55 +1000

    Add documentation

---
+++ R/data_documentation.R
@@ -0,0 +1,16 @@
+#' @title Victorian taxation revenue data
+#' @description Tidy data set of Victorian taxation revenue data
+#' @format A data frame with `r nrow(victax_tbl)` rows and `r ncol(victax_tbl)` variables:
+#' \describe{
+#'   \item{\code{tax_line}}{The name of the tax line}
+#'   \item{\code{tax_sub}}{Sub taxes where tax levies reported separetly}
+#'   \item{\code{publication_type}}{The budget, budget update or pre-election budget update publication type}
+#'   \item{\code{publication_year}}{The year the budget, budget update or pre-election budget update was for}
+#'   \item{\code{financial_year}}{The financial year of the data, as a character variable.}
+#'   \item{\code{fy_date}}{The financial year of the data, as a date variable.}
+#'   \item{\code{estimate}}{A numeric being the amount of revenue in $ million.}
+#'   \item{\code{estimate_type}}{Whether the data is an estimate or an actual value.}
+#'   
+#'}
+#' @source \url{https://www.dtf.vic.gov.au/state-taxation-revenue}
+"victax_tbl"
